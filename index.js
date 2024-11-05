a817d35508ade385080b946a603a6e37bc99da80
Ahmad0301 <sehroz3040@gmail.com>
2024-11-05 21:13:24 +0500

    creat get API

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const express = require('express')
 const app =express()
 const port= 2000
 app.use(express.json())
+let items = [
+    { id: 1, name: "Book 1", description: "This is the first book." },
+    { id: 2, name: "Book 2", description: "This is the second book." }
+  ];
 app.listen(port,()=>{
     console.log("server is running")
 })
+
+app.get('/items', (req, res) => {
+    res.json(items);
+  });
+  
+  app.get('/items/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const item = items.find(i => i.id === id);
+    if (item) {
+      res.json(item);
+    } else {
+      res.status(404).json({ message: "Item not found" });
+    }
+  });
+  
